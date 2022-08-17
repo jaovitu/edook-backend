87f71787ea5e4d50817c1f93d3ead3b971df32c9
jaovitu <jvgomes.dev@gmail.com>
2022-08-17 14:31:59 -0300

    Implementando as regras de negócio para a publicação de um livro

--- src/app/controllers/BookController.js
+++ src/app/controllers/BookController.js
@@ -1,12 +1,24 @@
+import dotenv from 'dotenv';
+import aws from 'aws-sdk';
+
 import BooksRepository from "../repositories/BooksRepository.js";
+
+dotenv.config();
+
+const S3 = new aws.S3();
 
 class BookController {
   async store(request, response) {
-    const { teste } = request.body;
+    const { title, author, description, genre, userID } = request.body;
+    const { location, key } = request.file;
 
-    console.log(request.file);
+    if ( !title || !author || !genre || !userID ) {
+      S3.deleteObject({ Bucket: 'edookimages', Key: key }).promise();
 
-    response.json({ ok: teste });
+      return response.status(400).json({ error: 'Missing required parameters!' });
+    }
+
+    response.json({ title, author, description, genre, imageURL: location, userID, key });
   }
 }
 

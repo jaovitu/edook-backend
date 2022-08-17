2f69cc4b41ddc1eadf823b8914955906f65baf66
jaovitu <jvgomes.dev@gmail.com>
2022-08-17 02:45:09 -0300

    Configurando e testando o upload para a aws

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,6 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import multer from "multer";
 import crypto from 'crypto';
 import path from 'path';
+
+import multerS3 from 'multer-s3';
+import aws from 'aws-sdk';
 
 import { fileURLToPath } from 'url';
 
@@ -9,20 +15,23 @@ const __dirname = path.dirname(__filename);
 
 const uploadsFolder = path.resolve(__dirname, '..', '..', 'tmp', 'uploads');
 
+const awsS3 = multerS3({
+  s3: new aws.S3(),
+  bucket: 'edookimages',
+  contentType: multerS3.AUTO_CONTENT_TYPE,
+  acl: 'public-read',
+
+  key: (request, file, callback) => {
+    const filenameHash = crypto.randomBytes(16).toString('hex');
+    const filename = `${filenameHash}-${file.originalname}`;
+
+    return callback(null, filename);
+  }
+});
+
 export default {
   directory: uploadsFolder,
-  storage: multer.diskStorage({
-    destination: (request, file, callback) => {
-      callback(null, uploadsFolder);
-    },
-
-    filename: (request, file, callback) => {
-      const filenameHash = crypto.randomBytes(16).toString('hex');
-      const filename = `${filenameHash}-${file.originalname}`;
-
-      return callback(null, filename);
-    }
-  }),
+  storage: awsS3,
 
   fileFilter: (request, file, callback) => {
     const allowedMimes = [ 'image/jpeg', 'image/pjpeg', 'image/png' ];

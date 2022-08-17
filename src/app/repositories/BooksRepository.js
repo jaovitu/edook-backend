1fd4a2aeb8e584bcc3e4543d0a8e028845f234cd
jaovitu <jvgomes.dev@gmail.com>
2022-08-17 16:02:38 -0300

    Implementando o repository de listagem de livros por gênero

--- src/app/repositories/BooksRepository.js
+++ src/app/repositories/BooksRepository.js
@@ -13,6 +13,17 @@ class BooksRepository {
     return rows;
   }
 
+  async findByGenre({ genre }) {
+    const rows = await db.query(`
+      SELECT books.*, users.name AS user_name, users.phone AS user_phone, addresses.city AS user_city, addresses.state AS user_state
+      FROM books
+      LEFT JOIN users ON books.user_id = users.id
+      LEFT JOIN addresses ON books.user_id = addresses.user_id
+      WHERE books.genre = $1
+      ORDER BY books.title
+  `, [genre]);
+  }
+
   async create({ title, author, description, genre, imageURL, awsImageKey, userID }) {
     const [ row ] = await db.query(`
       INSERT INTO books (title, author, description, genre, image_url, aws_image_key, user_id)
